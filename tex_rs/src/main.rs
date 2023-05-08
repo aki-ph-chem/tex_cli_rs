f1c67c4b604db09cb6753a0579843b3d68d7ffb3
aki <aki@mymail.com>
2023-05-08 21:49:30 +0900

    とりあえずコマンドライン引数から著者名、タイトルを読み取るよう機能を実装

---
+++ tex_rs/src/main.rs
@@ -0,0 +1,40 @@
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// Struct to get authour name, title from command line option
+pub struct CmdOpt {
+    /// authour name
+    #[argh(option, short = 'a' , default = "String::from(\"Authour\")")] 
+    pub authour: String,
+
+    /// title 
+    #[argh(option, short = 't', default = "String::from(\"Title\")")]
+    pub title: String,
+}
+
+/// Struct to hold author name and title
+#[derive(Debug)]
+pub struct Paper {
+    /// authour name
+    authour: String,
+
+    /// tittle of paper
+    title: String, 
+}
+
+impl Paper {
+    /// constructor
+    pub fn new(cmd_ops: CmdOpt) -> Paper {
+        Paper {
+            authour: cmd_ops.authour,
+            title: cmd_ops.title,
+        }
+    }
+}
+
+fn main() {
+    let cmd_ops: CmdOpt = argh::from_env();
+
+    let papaer = Paper::new(cmd_ops);
+    println!("papaer = {:?}", papaer);
+}
