70b18b15d61ed10e339e5f51cbc1049cf8ede7aa
aki <aki@mymail.com>
2023-05-09 17:08:05 +0900

    LaTexのテンプレート、Makefileの生成する関数を実装した

--- tex_rs/src/lib.rs
+++ tex_rs/src/lib.rs
@@ -36,3 +36,94 @@ impl Paper {
     }
 }
 
+/// set author name and title to LaTex template from struct Paper
+pub fn gen_latex_template(paper: Paper) -> String {
+    let author = paper.authour;
+    let title = paper.title; 
+
+    format!(
+        r#"
+\documentclass[dvipdfmx]{{jsarticle}}
+
+% アンカーを作る
+%\usepackage[dvipdfmx]{{hyperref}}
+%\usepackage{{pxjahyper}}
+
+\begin{{document}}
+\title{{{}}}
+\author{{{}}}
+\thispagestyle{{empty}}
+\maketitle
+
+% 目次
+%\tableofcontents
+%\clearpage
+\end{{document}}"#,title,author)
+}
+
+/// generate Makefile for build pdf document
+pub fn gen_makfile() -> String {
+    r#"
+TEX = platex
+PDF = dvipdfmx
+f = main
+
+viewer = qpdfview
+#viewer = mupdf
+
+${f}.pdf : ${f}.dvi
+	${PDF} $<
+
+#${f}.dvi : ${f}.tex
+#	${TEX} $< 
+${f}.dvi : ${f}.tex
+	@(${TEX} -interaction=nonstopmode $< > /dev/null 2>&1); \
+		if [ $$? -eq 0 ]; then \
+		echo "compile 1 is successed!"; \
+		else \
+		echo -e "failure! please read ${f}.log"; \
+		fi
+	@(grep -q "Rerun to get" ${f}.log && platex -interaction=nonstopmode $< > /dev/null 2>&1); \
+		if [ $$? -eq 0 ]; then \
+		echo "compile 2 is successed!"; \
+		fi
+
+opn :
+	${viewer} ${f}.pdf &
+
+clean: 
+	rm *.aux *.dvi *.log *.pdf
+
+.PHONY : opn clean
+        "#.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gen_latex_template() {
+        let ans = r#"
+\documentclass[dvipdfmx]{jsarticle}
+
+% アンカーを作る
+%\usepackage[dvipdfmx]{hyperref}
+%\usepackage{pxjahyper}
+
+\begin{document}
+\title{Card Shop}
+\author{Jouji Handa}
+\thispagestyle{empty}
+\maketitle
+
+% 目次
+%\tableofcontents
+%\clearpage
+\end{document}"#.to_string();
+let paper = Paper{authour: "Jouji Handa".to_string(), title: "Card Shop".to_string()}; 
+let result = gen_latex_template(paper);
+
+assert_eq!(ans, result);
+    }
+}
