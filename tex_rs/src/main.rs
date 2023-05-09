f23bf49bea76c6754579460457d247e412685766
aki <aki@mymail.com>
2023-05-10 01:29:41 +0900

    テンプレートを生成するまで実装した

--- tex_rs/src/main.rs
+++ tex_rs/src/main.rs
@@ -1,6 +1,29 @@
+//!
+//! ## usega
+//!
+//!```
+//! $ target/debug/tex_rs my_project -a aki -t my_tittle 
+//!```
 use tex_rs;
+use std::io::Error;
 
-fn main(){
+fn main() -> Result<(),Error>{
     let cmd_ops: tex_rs::CmdOpt = argh::from_env();
-    let papaer = tex_rs::Paper::new(cmd_ops);
+
+    if let Some(project) = &cmd_ops.project {
+        println!("project name is  {}", project);
+        // mkdir project_name/src
+        tex_rs::mkdir::mkdir(&format!("{}/{}",project,"src"))?; 
+    } else {
+        eprintln!("Error: No project name");
+        std::process::exit(1);
+    }
+
+    let papaer = tex_rs::Paper::new(&cmd_ops);
+    tex_rs::mkdir::write_string(&format!("{}/{}",&cmd_ops.project.clone().unwrap(),"Makefile")
+                                ,&tex_rs::gen_makfile())?;
+    tex_rs::mkdir::write_string(&format!("{}/{}",&cmd_ops.project.clone().unwrap(),"main.tex")
+                                ,&tex_rs::gen_latex_template(papaer))?;
+
+    Ok(())
 }
