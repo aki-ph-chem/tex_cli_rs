833bfe067dab7c8646755b2baaaa9fde6e23bfe8
aki <aki@mymail.com>
2023-12-08 00:07:49 +0900

    main()の中身を書き直してunwrap()を取り除いた #9

--- tex_rs/src/main.rs
+++ tex_rs/src/main.rs
@@ -2,32 +2,38 @@
 //! ## usega
 //!
 //!```
-//! $ target/debug/tex_rs my_project -a aki -t my_tittle 
+//! $ target/debug/tex_rs my_project -a aki -t my_tittle
 //!```
-use tex_rs;
 use std::io::Error;
+use tex_rs;
 
-fn main() -> Result<(),Error>{
+fn main() -> Result<(), Error> {
     let cmd_ops: tex_rs::CmdOpt = argh::from_env();
 
     if let Some(project) = &cmd_ops.project {
         println!("project name is  {}", project);
         // mkdir project_name/src
-        tex_rs::mkdir::mkdir(&format!("{}/{}",project,"src"))?; 
+        tex_rs::mkdir::mkdir(&format!("{}/{}", project, "src"))?;
+
+        let papaer = tex_rs::Paper::new(&cmd_ops);
+        tex_rs::mkdir::write_string(
+            &format!("{}/{}", &project, "Makefile"),
+            &tex_rs::Paper::gen_makfile(),
+        )?;
+
+        tex_rs::mkdir::write_string(
+            &format!("{}/{}", &project, "build.sh"),
+            &tex_rs::Paper::gen_build_sh(),
+        )?;
+
+        tex_rs::mkdir::write_string(
+            &format!("{}/{}", &project, "main.tex"),
+            &papaer.gen_latex_template(),
+        )?;
     } else {
         eprintln!("Error: No project name");
         std::process::exit(1);
     }
-
-    let papaer = tex_rs::Paper::new(&cmd_ops);
-    tex_rs::mkdir::write_string(&format!("{}/{}",&cmd_ops.project.clone().unwrap(),"Makefile")
-                                ,&tex_rs::Paper::gen_makfile())?;
-
-    tex_rs::mkdir::write_string(&format!("{}/{}",&cmd_ops.project.clone().unwrap(),"build.sh")
-                                ,&tex_rs::Paper::gen_build_sh())?;
-
-    tex_rs::mkdir::write_string(&format!("{}/{}",&cmd_ops.project.clone().unwrap(),"main.tex")
-                                ,&papaer.gen_latex_template())?;
 
     Ok(())
 }
